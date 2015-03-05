91dc782efd0d1aca985a8ac1b94c9fa44b03919c
Xia Xiongjun <xxjapp@gmail.com>
2015-03-05 15:58:20 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 mkdir -p ~/tools
 cd ~/tools
 
-wget --no-check-certificate https://codeload.github.com/xxjapp/HBShell/zip/master -O /tmp/m
-unzip /tmp/m
+wget --no-check-certificate https://codeload.github.com/xxjapp/HBShell/zip/master -O /tmp/h
+unzip /tmp/h
 
 rm -rf HBShell
 mv HBShell-master HBShell
